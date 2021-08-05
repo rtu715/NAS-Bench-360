1815eb7dfd89c462a890cf56e9aec9fda424fab3
Renbo <rt2632@columbia.edu>
2021-08-05 15:49:02 +0800

    random densenas trial

---
+++ densenas/point/generate_random.py
@@ -0,0 +1,57 @@
+import pprint
+import importlib
+
+from configs.search_config import search_cfg
+from configs.imagenet_train_cfg import cfg
+from models import model_derived
+from models.dropped_model import Dropped_Network
+from tools import utils
+from tools.config_yaml import merge_cfg_from_file, update_cfg_from_cfg
+from tools.multadds_count import comp_multadds
+
+
+def generate_arch(task, net_type):
+
+    update_cfg_from_cfg(search_cfg, cfg)
+    if task in ['cifar10', 'cifar100']:
+        merge_cfg_from_file('configs/cifar_small_search_cfg_resnet.yaml', cfg)
+        input_shape = (3, 32, 32)
+
+    elif task in ['scifar100', 'smnist']:
+        merge_cfg_from_file('configs/spherical_search_cfg_resnet.yaml', cfg)
+        input_shape = (3, 60, 60) if task == 'scifar100' else (1, 60, 60)
+
+    elif task == 'ninapro':
+        merge_cfg_from_file('configs/ninapro_search_cfg_resnet.yaml', cfg)
+        input_shape = (1, 16, 52)
+
+    elif task == 'audio':
+        merge_cfg_from_file('configs/audio_search_cfg_resnet.yaml', cfg)
+        input_shape = (1, 96, 101)
+
+    else:
+        raise NotImplementedError
+
+    config = cfg
+    pprint.pformat(config)
+
+    SearchSpace = importlib.import_module('models.search_space_' + net_type).Network
+    ArchGenerater = importlib.import_module('run_apis.derive_arch_' + net_type, __package__).ArchGenerate
+    derivedNetwork = getattr(model_derived, '%s_Net' % net_type.upper())
+
+    super_model = SearchSpace(config.optim.init_dim, task, config)
+    arch_gener = ArchGenerater(super_model, config)
+    der_Net = lambda net_config: derivedNetwork(net_config, task=task,
+                                                     config=config)
+
+    betas, head_alphas, stack_alphas = super_model.display_arch_params()
+    derived_arch = arch_gener.derive_archs(betas, head_alphas, stack_alphas)
+    derived_arch_str = '|\n'.join(map(str, derived_arch))
+    derived_model = der_Net(derived_arch_str)
+    derived_flops = comp_multadds(derived_model, input_size=input_shape)
+    #derived_params = utils.count_parameters_in_MB(derived_model)
+    print("Derived Model Mult-Adds = %.2fMB" % derived_flops)
+    #print("Derived Model Num Params = %.2fMB" % derived_params)
+    print(derived_arch_str)
+
+    return derived_arch_str
