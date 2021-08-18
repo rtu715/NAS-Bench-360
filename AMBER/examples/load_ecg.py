f4602a25c52b928b36c2bb2ca2931441d08375c5
Renbo <rt2632@columbia.edu>
2021-08-18 15:15:49 -0400

    amber files

---
+++ AMBER/examples/load_ecg.py
@@ -0,0 +1,177 @@
+import os
+import numpy as np
+import pandas as pd
+import scipy.io
+import pickle
+from sklearn.model_selection import train_test_split
+from collections import Counter
+from tensorflow.keras import backend as K
+from sklearn.metrics import classification_report, confusion_matrix
+
+def read_data_physionet_4_with_val(path, window_size=1000, stride=500):
+
+    # read pkl
+    with open(os.path.join(path,'challenge2017.pkl'), 'rb') as fin:
+        res = pickle.load(fin)
+    ## scale data
+    all_data = res['data']
+    for i in range(len(all_data)):
+        tmp_data = all_data[i]
+        tmp_std = np.std(tmp_data)
+        tmp_mean = np.mean(tmp_data)
+        all_data[i] = (tmp_data - tmp_mean) / tmp_std
+    ## encode label
+    all_label = []
+    for i in res['label']:
+        if i == 'N':
+            all_label.append(0)
+        elif i == 'A':
+            all_label.append(1)
+        elif i == 'O':
+            all_label.append(2)
+        elif i == '~':
+            all_label.append(3)
+    all_label = np.array(all_label)
+
+    # split train val test
+    X_train, X_test, Y_train, Y_test = train_test_split(all_data, all_label, test_size=0.2, random_state=0)
+    X_val, X_test, Y_val, Y_test = train_test_split(X_test, Y_test, test_size=0.5, random_state=0)
+
+    # slide and cut
+    print('before: ')
+    print(Counter(Y_train), Counter(Y_val), Counter(Y_test))
+    X_train, Y_train = slide_and_cut(X_train, Y_train, window_size=window_size, stride=stride)
+    X_val, Y_val, pid_val = slide_and_cut(X_val, Y_val, window_size=window_size, stride=stride, output_pid=True)
+    X_test, Y_test, pid_test = slide_and_cut(X_test, Y_test, window_size=window_size, stride=stride,
+                                             output_pid=True)
+    print('after: ')
+    print(Counter(Y_train), Counter(Y_val), Counter(Y_test))
+
+    # shuffle train
+    shuffle_pid = np.random.permutation(Y_train.shape[0])
+    X_train = X_train[shuffle_pid]
+    Y_train = Y_train[shuffle_pid]
+
+    X_train = np.expand_dims(X_train, 2)
+    X_val = np.expand_dims(X_val, 2)
+    X_test = np.expand_dims(X_test, 2)
+
+
+    return X_train, Y_train, X_val, Y_val
+
+def read_data_physionet_4(path, window_size=1000, stride=500):
+
+    # read pkl
+    with open(os.path.join(path,'challenge2017.pkl'), 'rb') as fin:
+        res = pickle.load(fin)
+    ## scale data
+    all_data = res['data']
+    for i in range(len(all_data)):
+        tmp_data = all_data[i]
+        tmp_std = np.std(tmp_data)
+        tmp_mean = np.mean(tmp_data)
+        all_data[i] = (tmp_data - tmp_mean) / tmp_std
+    ## encode label
+    all_label = []
+    for i in res['label']:
+        if i == 'N':
+            all_label.append(0)
+        elif i == 'A':
+            all_label.append(1)
+        elif i == 'O':
+            all_label.append(2)
+        elif i == '~':
+            all_label.append(3)
+    all_label = np.array(all_label)
+
+    # split train val test
+    X_train, X_test, Y_train, Y_test = train_test_split(all_data, all_label, test_size=0.1, random_state=0)
+
+    # slide and cut
+    print('before: ')
+    print(Counter(Y_train), Counter(Y_val), Counter(Y_test))
+    X_train, Y_train = slide_and_cut(X_train, Y_train, window_size=window_size, stride=stride)
+    X_test, Y_test, pid_test = slide_and_cut(X_test, Y_test, window_size=window_size, stride=stride,
+                                             output_pid=True)
+    print('after: ')
+    print(Counter(Y_train), Counter(Y_test))
+
+    # shuffle train
+    shuffle_pid = np.random.permutation(Y_train.shape[0])
+    X_train = X_train[shuffle_pid]
+    Y_train = Y_train[shuffle_pid]
+
+    X_train = np.expand_dims(X_train, 2)
+    X_test = np.expand_dims(X_test, 2)
+
+
+    return X_train, Y_train, X_test, Y_test, pid_test
+
+def slide_and_cut(X, Y, window_size, stride, output_pid=False, datatype=4):
+    out_X = []
+    out_Y = []
+    out_pid = []
+    n_sample = X.shape[0]
+    mode = 0
+    for i in range(n_sample):
+        tmp_ts = X[i]
+        tmp_Y = Y[i]
+        if tmp_Y == 0:
+            i_stride = stride
+        elif tmp_Y == 1:
+            if datatype == 4:
+                i_stride = stride//6
+            elif datatype == 2:
+                i_stride = stride//10
+            elif datatype == 2.1:
+                i_stride = stride//7
+        elif tmp_Y == 2:
+            i_stride = stride//2
+        elif tmp_Y == 3:
+            i_stride = stride//20
+        for j in range(0, len(tmp_ts)-window_size, i_stride):
+            out_X.append(tmp_ts[j:j+window_size])
+            out_Y.append(tmp_Y)
+            out_pid.append(i)
+    if output_pid:
+        return np.array(out_X), np.array(out_Y), np.array(out_pid)
+    else:
+        return np.array(out_X), np.array(out_Y)
+
+### Define F1 measures: F1 = 2 * (precision * recall) / (precision + recall)
+def f1_score(y_true, y_pred, pid_test):
+    final_pred = []
+    final_gt = []
+    for i_pid in np.unique(pid_test):
+        tmp_pred = y_pred[pid_test == i_pid]
+        tmp_gt = y_true[pid_test == i_pid]
+        final_pred.append(Counter(tmp_pred).most_common(1)[0][0])
+        final_gt.append(Counter(tmp_gt).most_common(1)[0][0])
+
+    ## classification report
+    tmp_report = classification_report(final_gt, final_pred, output_dict=True)
+    print(confusion_matrix(final_gt, final_pred))
+    f1_score = (tmp_report['0']['f1-score'] + tmp_report['1']['f1-score'] + tmp_report['2']['f1-score'] +
+                tmp_report['3']['f1-score']) / 4
+
+    return f1_score
+
+def custom_f1(y_true, y_pred):
+    def recall_m(y_true, y_pred):
+        TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        Positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+        recall = TP / (Positives+K.epsilon())
+        return recall
+
+
+    def precision_m(y_true, y_pred):
+        TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        Pred_Positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+
+        precision = TP / (Pred_Positives+K.epsilon())
+        return precision
+
+    precision, recall = precision_m(y_true, y_pred), recall_m(y_true, y_pred)
+
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
