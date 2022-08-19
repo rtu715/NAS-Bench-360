15a1f06e946b4489ad7e2887e39b5afacfc6e8b6
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2022-08-18 21:26:01 -0500

    Notebooks for xgboost

--- xgboost/utils.py
+++ xgboost/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def dm_to_numpy(loader, n=None):
+    print(len(loader))
     data = [xy for xy in loader]
     x = np.vstack([xy[0] for xy in data])
     y = np.concatenate([xy[1] for xy in data])
@@ -9,5 +10,7 @@ def dm_to_numpy(loader, n=None):
     y = y[:n]
     n = x.shape[0]
     x = x.reshape(n, -1)
-    y = y.reshape(n, -1) # TODO double-check inverse operation for pred
+    y = y.reshape(n, -1)  # TODO double-check inverse operation for pred
+
+    print(x.shape, y.shape)
     return x, y
