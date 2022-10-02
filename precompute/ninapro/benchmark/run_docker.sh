58b439bfeb989c34db898642e701781c1b45ff5a
root <root@o184i079.local>
2022-10-02 20:55:54 +0000

    Update ninapro precompute

---
+++ precompute/ninapro/benchmark/run_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nvidia-docker run -it --rm \
+  --net="host" \
+  --shm-size=8g \
+  --ulimit memlock=-1 \
+  --ulimit stack=67108864 \
+  -e LD_LIBRARY_PATH='/workspace/install/lib/' \
+  -v /home/automldec/github.com/rtu715/NAS-Bench-360:/NAS-Bench-360 \
+  renbotu/nb360:precompute-ninapro /bin/bash
+  
